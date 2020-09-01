35ed951d464a4e1d822df96e4a708edc5e67ff91
isyumich <59385945+Isyumich@users.noreply.github.com>
2020-09-01 22:03:13 +0300

    Исправления замечаний наставника

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import RouteView from "./view/route.js";
 import MenuView from "./view/menu.js";
 import FilterView from "./view/filter.js";
-import NoPointsMessageView from "./view/no-points-message.js";
 import {generateTrip} from "./mock/trip.js";
 import {getAscendingSortedArray} from "./view/utils/common.js";
 import {render, RenderPosition} from "./view/utils/trip.js";
-import BoardPresenter from "./presenter/trip";
+import BoardPresenter from "./presenter/tripBoard";
 
 const ELEMENT_COUNT = 15;
 
@@ -27,12 +26,7 @@ render(tripControlElement, new FilterView(), RenderPosition.BEFOREEND);
 
 const pageMainContainer = document.querySelector(`.page-main`);
 const tripEventsSection = pageMainContainer.querySelector(`.trip-events`);
-const tripEventsHeader = tripEventsSection.querySelector(`h2`);
 
-const boardPresenter = new BoardPresenter(tripEventsHeader, tripEventsSection);
+const boardPresenter = new BoardPresenter(tripEventsSection);
 
-if (sortedTrips.length === 0) {
-  render(tripEventsSection, new NoPointsMessageView(), RenderPosition.BEFOREEND);
-} else {
-  boardPresenter.init(tripEventsSection, sortedTrips);
-}
+boardPresenter.init(sortedTrips);
--- src/presenter/tripBoard.js
+++ src/presenter/tripBoard.js
@@ -3,25 +3,29 @@ import TripDaysItemView from "../view/trip-days-item.js";
 import TripEventsItemView from "../view/trip-events-item.js";
 import NewEventItemView from "../view/new-event-item.js";
 import SortingView from "../view/sorting.js";
+import NoPointsMessageView from "../view/no-points-message.js";
 import {render, RenderPosition, replace} from "../view/utils/trip.js";
 
 const ELEMENT_COUNT = 15;
 
-export default class Board {
-  constructor(eventsHeader) {
-    this._eventsHeader = eventsHeader;
+export default class TripBoard {
+  constructor(tripEventsSection) {
+    this._tripEventsSection = tripEventsSection;
 
     this._daysListComponent = new TripDaysListView();
     this._sortingComponent = new SortingView();
+    this._noPointsComponent = new NoPointsMessageView();
   }
 
-  init(tripEventsSection, trips) {
+  init(trips) {
+    this._trips = trips.slice();
     this._renderSort();
-    this._renderTripBoard(tripEventsSection, trips);
+    this._renderTripBoard(this._tripEventsSection, this._trips);
   }
 
   _renderSort() {
-    render(this._eventsHeader, this._sortingComponent, RenderPosition.AFTEREND);
+    const tripEventsHeader = this._tripEventsSection.querySelector(`h2`);
+    render(tripEventsHeader, this._sortingComponent, RenderPosition.AFTEREND);
   }
 
   _renderTrip(tripListElement, trip) {
@@ -58,15 +62,19 @@ export default class Board {
   }
 
   _renderTripBoard(tripEventsSection, trips) {
-    const tripDaysList = this._daysListComponent;
+    if (trips.length === 0) {
+      render(tripEventsSection, this._noPointsComponent, RenderPosition.BEFOREEND);
+    } else {
+      const tripDaysList = this._daysListComponent;
 
-    render(tripEventsSection, tripDaysList, RenderPosition.BEFOREEND);
+      render(tripEventsSection, tripDaysList, RenderPosition.BEFOREEND);
 
-    for (let i = 0; i < ELEMENT_COUNT; i++) {
-      if (i === 0 || trips[i].startTime !== trips[i - 1].startTime) {
-        const tripDayItem = new TripDaysItemView(i, trips[i]);
-        render(tripDaysList, tripDayItem, RenderPosition.BEFOREEND);
-        this._renderTrip(tripDayItem.getElement().querySelector(`.trip-events__list`), trips[i]);
+      for (let i = 0; i < ELEMENT_COUNT; i++) {
+        if (i === 0 || trips[i].startTime !== trips[i - 1].startTime) {
+          const tripDayItem = new TripDaysItemView(i, trips[i]);
+          render(tripDaysList, tripDayItem, RenderPosition.BEFOREEND);
+          this._renderTrip(tripDayItem.getElement().querySelector(`.trip-events__list`), trips[i]);
+        }
       }
     }
   }
